28b37c08bcf439aa3861eee822368c92b549681f
V-A-19 <64158804+V-A-19@users.noreply.github.com>
2020-05-20 14:12:49 +0300

    Add files via upload

---
+++ .github/workflows/new_table.sql
@@ -0,0 +1,14 @@
+create table testdb.if_prop
+(
+	username varchar(255),
+    prop1 boolean,
+    prop2 boolean,
+    prop3 boolean,
+    prop4 boolean,
+    prop5 boolean,
+    prop6 boolean,
+    prop7 boolean,
+    prop8 boolean,
+    prop9 boolean,
+    prop10 boolean
+);
